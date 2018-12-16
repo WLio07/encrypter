914cc2c3ab67c30e41d83323b6121c26ab1d2063
WLio07 <38637075+WLio07@users.noreply.github.com>
2018-12-16 21:32:07 -0200

    Update threedotscartesianplaneen.py
    added commentaries

--- threedotscartesianplaneen.py
+++ threedotscartesianplaneen.py
@@ -9,11 +9,11 @@ for i,u in zip(lpdph0, range(0, 3)):
 	x = float(((abs(lcoord[i] - lcoord[u])**2) + (abs((lcoord[u + 3]) - (lcoord[lpdph2[u]]))**2))**(1/2))
 	ldistancias.append(x)
 #the three dots connect, forming a triangle
-perimetro_triang = sum(ldistancias)
+perimetro_triang = sum(ldistancias) #triangle's perimeter
 sp = perimetro_triang/2   #sp == semiperimeter
-area_triang = abs((sp*(sp - ldistancias[0])*(sp - ldistancias[1])*(sp - ldistancias[2])))**(1/2)
+area_triang = abs((sp*(sp - ldistancias[0])*(sp - ldistancias[1])*(sp - ldistancias[2])))**(1/2) #triangle's area
 print("u.l. = unity of lenght", "u.a.² = unity of area", "The distance between A and B in u.l. is {}".format(ldistancias[0]), "The distance between A and C in u.l. is {}".format(ldistancias[1]), "The distance between B and C in u.l. is {}".format(ldistancias[2]), sep='\n')
-if (perimetro_triang!=0):
+if (perimetro_triang!=0): 
 	if(area_triang>0):
 		print("Upon connecting these three dots, a triangle will be formed.", "This triangle will have:", "Semiperimeter of {} u.l..".format(sp), "Perimeter of {} u.l..".format(perimetro_triang), "Area of {} u.a.².".format(area_triang), "Side AB of {} u.l..".format(ldistancias[0]), "Side AC of {} u.l..".format(ldistancias[1]), "Side BC of {} u.l..".format(ldistancias[2]), sep='\n')
 	elif (area_triang==0):
