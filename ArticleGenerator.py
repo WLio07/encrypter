114d7d3a6ac7a1db2ec6df26b70c01510319fc4f
William Lio <38637075+WillLio@users.noreply.github.com>
2019-07-12 00:21:33 -0300

    Update ArticleGenerator.py

--- ArticleGenerator.py
+++ ArticleGenerator.py
@@ -36,127 +36,45 @@ anything in context happened after the news (the schedulement of an event after
 the announcement of a new season etc)? ")
 afterwards1 = sys.stdin.readlines()
 
-
-
 src1 = input("Who
 made the announcement? ")
 
 adcj_temp = ('Recently','These days', 'Some days ago', 'In ' + months[datan[3:5]] + ' ' + datan[6:8] + 'st' if int(datan[7:8])==1 else 'nd' if int(datan[7:8])==2 else 'rd' if int(datan[7:8])==3 else 'th', 'Today' if datto=dattan)
 months = {'01': 'January', '02': 'February', '03': 'March', '04': 'April', '05': 'May', '06': 'June', '07': 'July', '08': 'August', '09': 'September', '10': 'October', '11': 'November', '12': 'December'}
-
-
-
 verbs, indicative_of_many, pos_adjec, indicative_of_airing = ('announced', 'revealed', 'put out', 'released', 'reported'), ('a series', 'a barrage', 'a streak', 'a chain', 'a plethora', 'multiple', 'various'), ('exhilarating', 'breathtaking', 'exciting', 'thrilling', 'uplifiting'), ('aired', 'broadcasted', 'went live', 'televised')
-
-
-
-ÃÂ 
-
-
 
 obj3, beforewards1, afterwards1 = ''.join(str(i.capitalize()) for i in obj3), ''.join(str(i.capitalize()) for i in beforewards1), ''.join(str(i.capitalize()) for i in
 afterwards1)
-
-
-
-ÃÂ 
-
-
-
 obj3, beforewards1, afterwards1 = re.split('\n', obj3), re.split('\n', beforewards1), re.split('\n',
 afterwards1)
 
 
-
-ÃÂ 
-
-
-
 def module_1(a, b):
 
-
-
-ÃÂ ÃÂ ÃÂ  name_apre = input("What's the name of the anime? ")
-
-
-
-ÃÂ ÃÂ ÃÂ  name_scrap = input("Anime/manga no namae wa? ")
-
-
-
-ÃÂ ÃÂ ÃÂ  return [a, ] #a is random of adcj_temp
-
-
-
-ÃÂ 
-
-
+    name_apre = input("What's the name of the anime? ")
+    name_scrap = input("Anime/manga no namae wa? ")
+    return [a, ] #a is random of adcj_temp
 
 def module_2():
-
-
-
-ÃÂ ÃÂ ÃÂ  pass
-
-
-
-ÃÂ 
+    pass
 
 
 
 def module_2_1():
-
-
-
-ÃÂ ÃÂ ÃÂ  a = int(input('Was it: 1 - a license purchase or 2 - a brand new product?'))
-
-
-
-ÃÂ ÃÂ ÃÂ  return a
-
-
-
-ÃÂ 
-
-
+    a = int(input('Was it: 1 - a license purchase or 2 - a brand new product?'))
+    return a
 
 def module_3():
-
-
-
-ÃÂ ÃÂ ÃÂ  pass
-
-
-
-ÃÂ 
-
-
+    pass
 
 def check_airing(line):
-
-
-
-ÃÂ ÃÂ ÃÂ  if 'episode' or ''ÃÂ  in line:
-
-
-
-ÃÂ ÃÂ ÃÂ ÃÂ ÃÂ ÃÂ ÃÂ  return True
-
-
-
-ÃÂ 
-
-
+    if 'episode' or '' in line:
+        return True
 
 def scraper(ob):
+    el = re.split(' ', ob)
 
-
-
-ÃÂ ÃÂ ÃÂ  el = re.split(' ', ob)
-
-
-
-ÃÂ ÃÂ ÃÂ  #mangadex
+#mangadex
 scraper
 
 
